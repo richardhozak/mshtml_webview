ea04ba1e5b6ec02626bea576053115c71aa2fc3e
zxey <zxey@users.noreply.github.com>
2020-03-14 06:49:27 +0100

    Add IOleInPlaceObject interface

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use winapi::shared::guiddef::*;
 use winapi::shared::minwindef::*;
+use winapi::shared::ntdef::*;
 use winapi::shared::windef::*;
 use winapi::shared::winerror;
-use winapi::shared::ntdef::*;
+use winapi::shared::wtypesbase::*;
 use winapi::um::errhandlingapi::*;
 use winapi::um::libloaderapi::*;
 use winapi::um::objidl::{IMoniker, FORMATETC};
 use winapi::um::ole2::*;
-use winapi::um::winuser::*;
 use winapi::um::wingdi::LOGPALETTE;
-use winapi::shared::wtypesbase::*;
+use winapi::um::winuser::*;
 // use winapi::um::objidlbase::IEnumUnknown;
 use com::{com_interface, interfaces::iunknown::IUnknown, InterfacePtr};
 use std::ffi::OsStr;
-use std::os::raw::{c_void, c_char};
+use std::os::raw::{c_char, c_void};
 use std::os::windows::ffi::OsStrExt;
 use std::ptr;
 
@@ -56,7 +56,11 @@ pub trait IOleClientSite: IUnknown {
 pub trait IOleObject: IUnknown {
     unsafe fn set_client_site(&self, p_client_site: *mut c_void) -> HRESULT;
     unsafe fn get_client_site(&self, p_client_site: *mut *mut c_void) -> HRESULT;
-    unsafe fn set_host_names(&self, sz_container_app: *const c_char, sz_container_app: *const c_char) -> HRESULT;
+    unsafe fn set_host_names(
+        &self,
+        sz_container_app: *const c_char,
+        sz_container_obj: *const c_char,
+    ) -> HRESULT;
     unsafe fn close(&self, dw_save_option: DWORD) -> HRESULT;
     unsafe fn set_moniker(&self, dw_which_moniker: DWORD, pmk: *mut c_void);
     unsafe fn get_moniker(
@@ -65,14 +69,24 @@ pub trait IOleObject: IUnknown {
         dw_which_moniker: DWORD,
         ppmk: *mut *mut c_void,
     ) -> HRESULT;
-    unsafe fn init_from_data(&self, p_data_object: *mut c_void, f_creation: BOOL, dw_reserved: DWORD) -> HRESULT;
-    unsafe fn get_clipboard_data(&self, dw_reserved: DWORD, pp_data_object: *mut *mut c_void) -> HRESULT;
+    unsafe fn init_from_data(
+        &self,
+        p_data_object: *mut c_void,
+        f_creation: BOOL,
+        dw_reserved: DWORD,
+    ) -> HRESULT;
+    unsafe fn get_clipboard_data(
+        &self,
+        dw_reserved: DWORD,
+        pp_data_object: *mut *mut c_void,
+    ) -> HRESULT;
     unsafe fn do_verb(&self, i_verb: LONG, lpmsg: LPMSG, p_active_site: *mut c_void) -> HRESULT;
     unsafe fn enum_verbs(&self, pp_enum_ole_verb: *mut *mut c_void) -> HRESULT;
     unsafe fn update(&self) -> HRESULT;
     unsafe fn is_up_to_date(&self) -> HRESULT;
     unsafe fn get_user_class_id(&self, p_clsid: *mut CLSID) -> HRESULT;
-    unsafe fn get_user_type(&self, dw_form_of_type: DWORD, psz_user_type: *mut LPOLESTR) -> HRESULT;
+    unsafe fn get_user_type(&self, dw_form_of_type: DWORD, psz_user_type: *mut LPOLESTR)
+        -> HRESULT;
     unsafe fn set_extent(&self, dw_draw_aspect: DWORD, psizel: *mut SIZEL) -> HRESULT;
     unsafe fn get_extent(&self, dw_draw_aspect: DWORD, psizel: *mut SIZEL) -> HRESULT;
     unsafe fn advise(&self, p_advise_sink: *mut c_void, pdw_connection: *mut DWORD) -> HRESULT;
@@ -82,8 +96,23 @@ pub trait IOleObject: IUnknown {
     unsafe fn set_color_scheme(&self, p_logpal: *mut LOGPALETTE) -> HRESULT;
 }
 
+#[com_interface(00000114-0000-0000-C000-000000000046)]
+pub trait IOleWindow: IUnknown {
+    unsafe fn get_window(&self, phwnd: *mut HWND) -> HRESULT;
+    unsafe fn context_sensitive_help(&self, f_enter_mode: BOOL) -> HRESULT;
+}
+
+#[com_interface(00000113-0000-0000-C000-000000000046)]
+pub trait IOleInPlaceObject: IOleWindow {
+    unsafe fn in_place_deactivate(&self) -> HRESULT;
+    unsafe fn ui_deactivate(&self) -> HRESULT;
+    unsafe fn set_object_rects(&self, lprc_pos_rect: LPCRECT, lprc_clip_rect: LPCRECT) -> HRESULT;
+    unsafe fn reactivate_and_undo(&self) -> HRESULT;
+}
+
 struct WebBrowser {
-    ole_object: InterfacePtr<dyn IOleObject>
+    ole_object: InterfacePtr<dyn IOleObject>,
+    ole_in_place_object: InterfacePtr<dyn IOleInPlaceObject>,
 }
 
 // #[com_interface(0000011b-0000-0000-C000-000000000046)]
@@ -157,16 +186,18 @@ extern "system" fn wndproc(hwnd: HWND, message: UINT, wparam: WPARAM, lparam: LP
         WM_CREATE => {
             println!("created");
             1
-        },
+        }
         WM_SIZE => {
             println!("size");
             1
-        },
+        }
         WM_DESTROY => {
-            unsafe { ExitProcess(0); }
+            unsafe {
+                ExitProcess(0);
+            }
             1
-        },
-        _ => unsafe { DefWindowProcW(hwnd, message, wparam, lparam) }
+        }
+        _ => unsafe { DefWindowProcW(hwnd, message, wparam, lparam) },
     }
 }
 
