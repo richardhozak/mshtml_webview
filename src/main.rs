6bf6b5c9911ff3b732b18a12259d80e340138bc6
zxey <zxey@users.noreply.github.com>
2020-03-14 06:36:33 +0100

    Add IOleObject interface

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 use winapi::shared::guiddef::*;
 use winapi::shared::minwindef::*;
-use winapi::shared::windef::HWND;
-use winapi::shared::winerror::*;
+use winapi::shared::windef::*;
+use winapi::shared::winerror;
+use winapi::shared::ntdef::*;
 use winapi::um::errhandlingapi::*;
 use winapi::um::libloaderapi::*;
 use winapi::um::objidl::{IMoniker, FORMATETC};
 use winapi::um::ole2::*;
 use winapi::um::winuser::*;
+use winapi::um::wingdi::LOGPALETTE;
+use winapi::shared::wtypesbase::*;
 // use winapi::um::objidlbase::IEnumUnknown;
-use com::{com_interface, interfaces::iunknown::IUnknown};
+use com::{com_interface, interfaces::iunknown::IUnknown, InterfacePtr};
 use std::ffi::OsStr;
-use std::os::raw::c_void;
+use std::os::raw::{c_void, c_char};
 use std::os::windows::ffi::OsStrExt;
 use std::ptr;
 
@@ -49,6 +52,40 @@ pub trait IOleClientSite: IUnknown {
     unsafe fn request_new_object_layout(&self) -> HRESULT;
 }
 
+#[com_interface(00000112-0000-0000-C000-000000000046)]
+pub trait IOleObject: IUnknown {
+    unsafe fn set_client_site(&self, p_client_site: *mut c_void) -> HRESULT;
+    unsafe fn get_client_site(&self, p_client_site: *mut *mut c_void) -> HRESULT;
+    unsafe fn set_host_names(&self, sz_container_app: *const c_char, sz_container_app: *const c_char) -> HRESULT;
+    unsafe fn close(&self, dw_save_option: DWORD) -> HRESULT;
+    unsafe fn set_moniker(&self, dw_which_moniker: DWORD, pmk: *mut c_void);
+    unsafe fn get_moniker(
+        &self,
+        dw_assign: DWORD,
+        dw_which_moniker: DWORD,
+        ppmk: *mut *mut c_void,
+    ) -> HRESULT;
+    unsafe fn init_from_data(&self, p_data_object: *mut c_void, f_creation: BOOL, dw_reserved: DWORD) -> HRESULT;
+    unsafe fn get_clipboard_data(&self, dw_reserved: DWORD, pp_data_object: *mut *mut c_void) -> HRESULT;
+    unsafe fn do_verb(&self, i_verb: LONG, lpmsg: LPMSG, p_active_site: *mut c_void) -> HRESULT;
+    unsafe fn enum_verbs(&self, pp_enum_ole_verb: *mut *mut c_void) -> HRESULT;
+    unsafe fn update(&self) -> HRESULT;
+    unsafe fn is_up_to_date(&self) -> HRESULT;
+    unsafe fn get_user_class_id(&self, p_clsid: *mut CLSID) -> HRESULT;
+    unsafe fn get_user_type(&self, dw_form_of_type: DWORD, psz_user_type: *mut LPOLESTR) -> HRESULT;
+    unsafe fn set_extent(&self, dw_draw_aspect: DWORD, psizel: *mut SIZEL) -> HRESULT;
+    unsafe fn get_extent(&self, dw_draw_aspect: DWORD, psizel: *mut SIZEL) -> HRESULT;
+    unsafe fn advise(&self, p_advise_sink: *mut c_void, pdw_connection: *mut DWORD) -> HRESULT;
+    unsafe fn unadvise(&self, dw_connection: DWORD) -> HRESULT;
+    unsafe fn enum_advise(&self, ppenum_advise: *mut *mut c_void) -> HRESULT;
+    unsafe fn get_misc_status(&self, dw_aspect: DWORD, pdw_status: *mut DWORD) -> HRESULT;
+    unsafe fn set_color_scheme(&self, p_logpal: *mut LOGPALETTE) -> HRESULT;
+}
+
+struct WebBrowser {
+    ole_object: InterfacePtr<dyn IOleObject>
+}
+
 // #[com_interface(0000011b-0000-0000-C000-000000000046)]
 // pub trait IOleContainer: IUnknown {
 //     unsafe fn enum_objects(&self, grf_flags: DWORD, ppenum: *mut *mut IEnumUnknown) -> HRESULT;
@@ -58,7 +95,7 @@ pub trait IOleClientSite: IUnknown {
 fn main() {
     unsafe {
         let result = OleInitialize(ptr::null_mut());
-        if result != S_OK && result != S_FALSE {
+        if result != winerror::S_OK && result != winerror::S_FALSE {
             panic!("could not initialize ole");
         }
 
